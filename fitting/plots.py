9fcc60319d3a5a2f85d2748a6c9dd32af7723687
Brendon J. Brewer <brendon.brewer@gmail.com>
2026-04-28 15:28:25 +1200

    All plots

--- fitting/plots.py
+++ fitting/plots.py
@@ -44,68 +44,68 @@ plt.savefig("cornerplot_flat.pdf", bbox_inches="tight")
 plt.show()
 
 
-#plt.figure(figsize=(14, 6))
+plt.figure(figsize=(14, 6))
 
-#plt.subplot(1, 2, 1)
-#posterior_sample = np.loadtxt("posterior_sample_flat.txt")
+plt.subplot(1, 2, 1)
+posterior_sample = np.loadtxt("results/posterior_sample_flat_free.txt")
 
-#mu = posterior_sample[:,0]
-#log10_sigma = posterior_sample[:,1]
-#log10_beta = posterior_sample[:,2]
-#log10_jitter = posterior_sample[:,3]
-#log10_tau = (log10_sigma - log10_beta + 0.5*np.log10(2))*2
+mu = posterior_sample[:,0]
+log10_sigma = posterior_sample[:,1]
+log10_beta = posterior_sample[:,2]
+log10_jitter = posterior_sample[:,3]
+log10_tau = (log10_sigma - log10_beta + 0.5*np.log10(2))*2
 
-#plt.hist(log10_tau, 40, density=True, alpha=0.3,
-#         label="Free $\\mu$")
-#plt.title("Flat Priors")
-#plt.xlim([1.0, 10.0])
-#plt.axvline(3.60749121, color="k", alpha=0.6, label="True value")
-#plt.legend()
-#plt.ylabel("Probability Density")
+plt.hist(log10_tau, 40, density=True, alpha=0.3,
+         label="Free $\\mu$")
+plt.title("Flat Priors")
+plt.xlim([1.0, 10.0])
+plt.axvline(3.60749121, color="k", alpha=0.6, label="True value")
+plt.ylabel("Probability Density")
 
-#posterior_sample = np.loadtxt("posterior_sample.txt")
+posterior_sample = np.loadtxt("results/posterior_sample_flat_fixed.txt")
 
-#mu = posterior_sample[:,0]
-#log10_sigma = posterior_sample[:,1]
-#log10_beta = posterior_sample[:,2]
-#log10_jitter = posterior_sample[:,3]
-#log10_tau = (log10_sigma - log10_beta + 0.5*np.log10(2))*2
+mu = posterior_sample[:,0]
+log10_sigma = posterior_sample[:,1]
+log10_beta = posterior_sample[:,2]
+log10_jitter = posterior_sample[:,3]
+log10_tau = (log10_sigma - log10_beta + 0.5*np.log10(2))*2
 
-#plt.hist(log10_tau, 40, density=True, alpha=0.3,
-#         label="Fixed $\\mu$")
+plt.hist(log10_tau, 40, density=True, alpha=0.3,
+         label="Fixed $\\mu$")
+plt.legend()
 
-#plt.subplot(1, 2, 2)
-#posterior_sample = np.loadtxt("posterior_sample_informative.txt")
+plt.subplot(1, 2, 2)
+posterior_sample = np.loadtxt("results/posterior_sample_informative_free.txt")
 
-#mu = posterior_sample[:,0]
-#log10_sigma = posterior_sample[:,1]
-#log10_beta = posterior_sample[:,2]
-#log10_jitter = posterior_sample[:,3]
-#log10_tau = (log10_sigma - log10_beta + 0.5*np.log10(2))*2
+mu = posterior_sample[:,0]
+log10_sigma = posterior_sample[:,1]
+log10_beta = posterior_sample[:,2]
+log10_jitter = posterior_sample[:,3]
+log10_tau = (log10_sigma - log10_beta + 0.5*np.log10(2))*2
 
-#plt.hist(log10_tau, 40, density=True, alpha=0.3,
-#         label="Free $\\mu$")
-#plt.title("Informative Priors")
-
-
-#posterior_sample = np.loadtxt("posterior_sample_informative2.txt")
-
-#mu = posterior_sample[:,0]
-#log10_sigma = posterior_sample[:,1]
-#log10_beta = posterior_sample[:,2]
-#log10_jitter = posterior_sample[:,3]
-#log10_tau = (log10_sigma - log10_beta + 0.5*np.log10(2))*2
-
-#plt.hist(log10_tau, 40, density=True, alpha=0.3,
-#         label="Fixed $\\mu$")
-#plt.title("Informative Priors")
+plt.hist(log10_tau, 40, density=True, alpha=0.3,
+         label="Free $\\mu$")
+plt.title("Informative Priors")
 
 
-#plt.xlabel("$\\log_{10}(\\tau/{\\rm days})$")
-#plt.xlim([1.0, 10.0])
-#plt.axvline(3.60749121, color="k", alpha=0.6, label="True value")
-#plt.legend()
+posterior_sample = np.loadtxt("results/posterior_sample_informative_fixed.txt")
 
-#plt.savefig("four_posteriors.pdf")
-#plt.show()
+mu = posterior_sample[:,0]
+log10_sigma = posterior_sample[:,1]
+log10_beta = posterior_sample[:,2]
+log10_jitter = posterior_sample[:,3]
+log10_tau = (log10_sigma - log10_beta + 0.5*np.log10(2))*2
+
+plt.hist(log10_tau, 40, density=True, alpha=0.3,
+         label="Fixed $\\mu$")
+plt.title("Informative Priors")
+
+
+plt.xlabel("$\\log_{10}(\\tau/{\\rm days})$")
+plt.xlim([1.0, 10.0])
+plt.axvline(3.60749121, color="k", alpha=0.6, label="True value")
+plt.legend()
+
+plt.savefig("four_posteriors.pdf")
+plt.show()
 
