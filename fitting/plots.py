f851e60ce9304d9d696b29651d04db46ad31905e
Brendon J. Brewer <brendon.brewer@gmail.com>
2026-04-30 13:40:03 +1200

    New plot

--- fitting/plots.py
+++ fitting/plots.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numpy.random as rng
 import matplotlib.pyplot as plt
 import corner
 
@@ -7,8 +8,9 @@ plt.rcParams.update({
     "font.size": 14,
 })
 
-
+rng.seed(123)
 ndim = 4
+
 
 def cornerplot(posterior_sample):
     mu = posterior_sample[:,0]
@@ -109,5 +111,56 @@ plt.axvline(3.7787833316317925, color="k", alpha=0.6, label="True value")
 plt.legend()
 
 plt.savefig("four_posteriors.pdf")
+plt.show()
+
+
+import sys
+import os
+
+# Directory containing mymodel.py
+_here = os.path.dirname(os.path.abspath(__file__))
+
+# Parent directory: NewCAR/
+_parent = os.path.abspath(os.path.join(_here, ".."))
+
+# Add parent to sys.path so we can import shared.py
+if _parent not in sys.path:
+    sys.path.insert(0, _parent)
+
+import shared
+from shared import *
+
+n = 1000000
+params = []
+for i in range(n):
+    params.append(prior_transform_informative(rng.rand(num_params)))
+params = np.vstack(params)
+
+log10_sigma = params[:,1]
+log10_eta   = params[:,2]
+log10_tau   = np.log10(2.0) + 2.0*(log10_sigma - log10_eta)
+plt.hist(log10_tau, density=True, bins=100, alpha=0.3, label="Informative Prior")
+plt.xlabel("$\\log_{10}(\\tau)$")
+plt.ylabel("Prior Density")
+print(np.mean(log10_tau), np.std(log10_tau))
+
+params = []
+for i in range(n):
+    params.append(prior_transform_flat(rng.rand(num_params)))
+params = np.vstack(params)
+
+log10_sigma = params[:,1]
+log10_eta   = params[:,2]
+log10_tau   = np.log10(2.0) + 2.0*(log10_sigma - log10_eta)
+plt.hist(log10_tau, density=True, bins=100, alpha=0.3, label="Flat Prior")
+plt.xlabel("$\\log_{10}(\\tau)$")
+plt.ylabel("Prior Density")
+print(np.mean(log10_tau), np.std(log10_tau))
+
+
+
+
+plt.legend()
+plt.savefig("tau_priors.pdf")
 plt.show()
 
