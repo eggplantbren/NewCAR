401c52de4265358f0e3799cabe15c6cc58314618
Brendon J. Brewer <brendon.brewer@gmail.com>
2026-05-19 10:22:22 +1200

    Corner plot

--- hierarchical/showresults.py
+++ hierarchical/showresults.py
@@ -1,2 +1,36 @@
 import dnest4.classic as dn4
-dn4.postprocess(rng_seed=0)
+import numpy as np
+import matplotlib.pyplot as plt
+import corner
+from mymodel import names
+
+dn4.postprocess()
+
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.size": 14,
+})
+
+
+posterior_sample = np.loadtxt("posterior_sample.txt")
+
+# Subset
+hypers = posterior_sample[:, 4:9]
+ndim = hypers.shape[1]
+
+figure = corner.corner(hypers,
+    labels=[r"$\beta_0$", r"$\beta_1$", r"$\beta_2$",
+            r"$n$", r"$s_{\log_{10}(\eta)}$"],
+            plot_contours=False,
+            plot_density=False, fontsize=14,
+            hist_kwargs={"color":"blue", "alpha":0.2, "histtype":"stepfilled",
+                         "edgecolor":"black","lw":"3"} )
+
+axes = np.array(figure.axes).reshape((ndim, ndim))
+
+for i in range(ndim):
+    ax = axes[i, i]
+
+plt.savefig("hierarchical_corner.png", dpi=400)
+plt.show()
+
